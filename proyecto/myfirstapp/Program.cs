90e1c6482c4e46317dd50f5ef57344e4656fced9
emmanuelbilkis <emmanuelbilkis@gmail.com>
2022-05-29 15:19:40 -0300

    agrego mifirst app

---
+++ proyecto/myfirstapp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el numero de cifras del numero : \n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escriba el nuemero que desea invertir\n");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(num % 10);
+                num = num / 10;
+            }
+        }
+    }
+
+}
+
+
