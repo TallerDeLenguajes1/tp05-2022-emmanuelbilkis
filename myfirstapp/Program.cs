20bf8c81bdbf03dd21ddcd822d5d7ace320668be
emmanuelbilkis <emmanuelbilkis@gmail.com>
2022-05-29 16:04:25 -0300

    modifico y finalizo invertir numeros

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -8,14 +8,21 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Escriba el numero de cifras del numero : \n");
             int n = Convert.ToInt32(Console.ReadLine());
-
+            int[] numeroInvertido = new int [n];
             Console.WriteLine("Escriba el nuemero que desea invertir\n");
             int num = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(num % 10);
+                numeroInvertido[i] = num%10;
                 num = num / 10;
+            }
+
+            Console.WriteLine("Numero invertido: \n");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numeroInvertido[i]);
             }
         }
     }
